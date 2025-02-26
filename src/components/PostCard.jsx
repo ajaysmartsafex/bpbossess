1a097bd665b6137a730d539a3e0b9adcb9c99526
ajaysmartsafex <ajaysmartsafex@gmail.com>
2025-02-26 18:54:54 +0530

    post card and scss fills

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -20,15 +20,15 @@ function PostCard({ $id, featuredImage, title, gamenumber, starttime, endtime, c
         <div className='flex w-full justify-center mb-4 post-image'>
           <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='rounded-xl' />
         </div>
-        <h1 className='text-xl font-bold'>{title}</h1>
+        <h1 className='game_name_color text-xl font-bold'>{title}</h1>
         <h2 className='text-xl font-bold text-black mt-2'>Game Number</h2>
-        <h2 className='text-xl font-bold'>{gamenumber}</h2>
+        <h2 className='game_number_color text-xl font-bold'>{gamenumber}</h2>
         <h2 className='text-xl font-bold text-black mt-2'>Game Time</h2>
         <div className='game_time'>
           <span className='start_time'>{starttime}</span>
           <span className='end_time'>{endtime}</span>
         </div>
-        <h2 className='text-xl font-bold red_text mt-2'>Game Result: {gameResult ? currentDate : 'No result'}</h2>
+        <h2 className='result_number text-xl font-bold red_text mt-2'>Result: {gameResult ? currentDate : 'No result'}</h2>
         <div className='final_result'>
           <span className='lrnumber red_text'>{displayNumbers}</span>
         </div>
